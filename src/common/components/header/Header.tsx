52b7abfad06e727082965ff7400c1d43fda8889c
Dachev <dachev1919@gmail.com>
2023-02-04 21:14:34 +0200

    Added auth hook and logout function

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,14 +1,15 @@
-import { FC } from "react";
+import {FC} from "react";
 import {NavLink} from "react-router-dom";
 import clsx from "clsx";
 import {Container} from "../container/Container";
 import {useAuth} from "../../../modules/auth/hooks/use-auth";
 
-interface HeaderProps {}
+interface HeaderProps {
+}
 
 export const Header: FC<HeaderProps> = () => {
     const {isLoggedIn, logOut} = useAuth();
-    const navLinkClasses = ({isActive}: {isActive: boolean}) => clsx("py-navItem transition-all hover:text-black/60 hover:no-underline", {
+    const navLinkClasses = ({isActive}: { isActive: boolean }) => clsx("py-navItem transition-all hover:text-black/60 hover:no-underline", {
         "text-black/80": isActive,
         "text-black/30": !isActive
     });
@@ -18,7 +19,8 @@ export const Header: FC<HeaderProps> = () => {
             <nav className="px-2 py-4">
                 <Container>
                     <div className="flex justify-between items-center">
-                        <NavLink className="font-titillium text-2xl mr-8 text-conduit-green" to="/conduit ">conduit</NavLink>
+                        <NavLink className="font-titillium text-2xl mr-8 text-conduit-green"
+                                 to="/conduit ">conduit</NavLink>
                         <ul className="flex gap-4 pl-0 m-0 list-none">
                             <li>
                                 <NavLink className={navLinkClasses} to="/conduit" end>Home</NavLink>
@@ -26,10 +28,11 @@ export const Header: FC<HeaderProps> = () => {
                             {isLoggedIn ? (
                                 <>
                                     <li>
-                                        <NavLink className={navLinkClasses} to="/conduit" onClick={logOut}>Log Out</NavLink>
+                                        <NavLink className={navLinkClasses} to="/conduit" onClick={logOut}>Log
+                                            Out</NavLink>
                                     </li>
                                 </>
-                                ) : (
+                            ) : (
                                 <>
                                     <li>
                                         <NavLink className={navLinkClasses} to="/conduit/sign-in">Sign In</NavLink>
@@ -37,7 +40,7 @@ export const Header: FC<HeaderProps> = () => {
                                     <li>
                                         <NavLink className={navLinkClasses} to="/conduit/sign-up">Sign Up</NavLink>
                                     </li>
-                                 </>
+                                </>
                             )}
                         </ul>
                     </div>
